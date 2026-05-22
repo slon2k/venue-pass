14d78831a18a611b79a8555a7d4837e2ba9ce77d
Boris Korobeinikov <slon2k@gmail.com>
2026-05-22 15:08:15 +0500

    feat: add OutboxMessageConfiguration for entity persistence in Events module

--- src/VenuePass.Modules.Events/Infrastructure/EventsDbContext.cs
+++ src/VenuePass.Modules.Events/Infrastructure/EventsDbContext.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using VenuePass.Modules.Events.Domain.Venues;
+using VenuePass.Modules.Events.Infrastructure.Outbox;
 
 namespace VenuePass.Modules.Events.Infrastructure;
 
 public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) : DbContext(options)
 {
     public const string Schema = "events";
+
     public DbSet<Venue> Venues => Set<Venue>();
+
+    public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
